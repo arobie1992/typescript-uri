3d22c8f4366c4a1cc5feb6eb54a6d0f991448254
arobie1992 <55855135+arobie1992@users.noreply.github.com>
2023-01-31 14:15:02 -0500

    No readonly workaround
    Switching to nested class to parse uri so don't have to use janky readonly workaround.

--- Uri.ts
+++ Uri.ts
@@ -1,142 +1,174 @@
-type Writeable = { -readonly [P in keyof Uri]: Uri[P] };
-
 class Uri {
-    private static URL_REGEX = /^(([^:/?#]+):)?(\/\/([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/;
+    readonly uriStr: string;
 
-    readonly uri: string;
+    readonly scheme: string | null;
+    readonly authority: string | null;
+    readonly path: string;
+    readonly query: string | null;
+    readonly fragment: string | null;
 
-    readonly scheme: string | null = null;
-    readonly authority: string | null = null;
-    readonly path: string = "";
-    readonly query: string | null = null;
-    readonly fragment: string | null = null;
+    readonly user: string | null;
+    readonly password: string | null;
+    readonly host: string | null;
+    readonly port: string | null;
 
-    readonly user: string | null = null;
-    readonly password: string | null = null;
-    readonly host: string | null = null;
-    readonly port: string | null = null;
+    readonly pathSegments: ReadonlyArray<string>;
 
-    readonly pathSegments: ReadonlyArray<string> = [];
-
-    readonly queryParameters: ReadonlyMap<string, ReadonlyArray<string|null>> = new Map();
+    readonly queryParameters: ReadonlyMap<string, ReadonlyArray<string|null>>;
 
     constructor(uri: string) {
-        this.uri = uri;
-        this.parseUri();
-        this.parseAuthority();
-        this.parsePath();
-        this.parseQuery();
+        this.uriStr = uri;
+        const parseResult = new Uri.ParseResult(this.uriStr);
+        this.scheme = parseResult.scheme;
+        this.authority = parseResult.authority;
+        this.path = parseResult.path;
+        this.query = parseResult.query;
+        this.fragment = parseResult.fragment;
+
+        this.user = parseResult.user;
+        this.password = parseResult.password;
+        this.host = parseResult.host;
+        this.port = parseResult.port;
+
+        this.pathSegments = parseResult.pathSegments;
+        this.queryParameters = parseResult.queryParameters;
     }
 
-    private parseUri() {
-        const matches = this.uri.match(Uri.URL_REGEX);
-        if(!matches) {
-            throw Error("Invalid URI"); 
-        }
-        (this as Writeable).scheme = matches[2] ?? null;
-        (this as Writeable).authority = matches[4] ?? null;
-        (this as Writeable).path = matches[5] ?? null;
-        (this as Writeable).query = matches[7] ?? null;
-        (this as Writeable).fragment = matches[9] ?? null;
-    }
+    public toString = () => this.uriStr;
 
-    private parseAuthority() {
-        if(!this.authority) {
-            return;
+    private static ParseResult = class {
+        private static URL_REGEX = /^(([^:/?#]+):)?(\/\/([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/;
+        uri: string;
+
+        scheme: string | null = null;
+        authority: string | null = null;
+        path: string = "";
+        query: string | null = null;
+        fragment: string | null = null;
+
+        user: string | null = null;
+        password: string | null = null;
+        host: string | null = null;
+        port: string | null = null;
+
+        pathSegments: Array<string> = [];
+
+        queryParameters: Map<string, Array<string|null>> = new Map();
+
+        constructor(uri: string) {
+            this.uri = uri;
+            this.parseUri();
+            this.parseAuthority();
+            this.parsePath();
+            this.parseQuery();
         }
 
-        enum State { HOST, PORT }
-        const falsy2Null = (str: string) => str ? str : null;
-        for(let start = 0, cur = 0, state = State.HOST, usepassSet = false;; cur++) {
-            if(cur == this.authority.length) {
-                let str = this.authority.substring(start, cur);
-                if(state == State.HOST) {
-                    (this as Writeable).host = falsy2Null(str);
-                } else {
-                    (this as Writeable).port = falsy2Null(str);
-                }
-                break;
+        private parseUri() {
+            const matches = this.uri.match(Uri.ParseResult.URL_REGEX);
+            if(!matches) {
+                throw Error("Invalid URI"); 
             }
-            switch(this.authority.charAt(cur)) {
-                case ':':
+            this.scheme = matches[2] ?? null;
+            this.authority = matches[4] ?? null;
+            this.path = matches[5] ?? null;
+            this.query = matches[7] ?? null;
+            this.fragment = matches[9] ?? null;
+        }
+
+        private parseAuthority() {
+            if(!this.authority) {
+                return;
+            }
+
+            enum State { HOST, PORT }
+            const falsy2Null = (str: string) => str ? str : null;
+            for(let start = 0, cur = 0, state = State.HOST, usepassSet = false;; cur++) {
+                if(cur == this.authority.length) {
+                    let str = this.authority.substring(start, cur);
                     if(state == State.HOST) {
-                        (this as Writeable).host = falsy2Null(this.authority.substring(start, cur));
-                        state = State.PORT;
-                        start = cur + 1;
+                        this.host = falsy2Null(str);
+                    } else {
+                        this.port = falsy2Null(str);
                     }
-                    // else it's a part of the potentially invalid port
                     break;
-                case '@':
-                    if(!usepassSet) {
-                        // turns out we've actually been parsing user and/or password
-                        usepassSet = true;
-                        if(state == State.PORT) {
-                            // we've seen a colon which caused us to think we were on port but were actually on password
-                            (this as Writeable).user = this.host;
-                            (this as Writeable).host = null;
-                            (this as Writeable).password = falsy2Null(this.authority.substring(start, cur));
-                        } else {
-                            // we're still on host, but is actually the user
-                            (this as Writeable).user = falsy2Null(this.authority.substring(start, cur));
+                }
+                switch(this.authority.charAt(cur)) {
+                    case ':':
+                        if(state == State.HOST) {
+                            this.host = falsy2Null(this.authority.substring(start, cur));
+                            state = State.PORT;
+                            start = cur + 1;
                         }
-                        state = State.HOST;
-                        start = cur + 1;
-                    }
-                    // else it's part of the possibly invalid host name or port
-                    break;
-                // else no special behavior
+                        // else it's a part of the potentially invalid port
+                        break;
+                    case '@':
+                        if(!usepassSet) {
+                            // turns out we've actually been parsing user and/or password
+                            usepassSet = true;
+                            if(state == State.PORT) {
+                                // we've seen a colon which caused us to think we were on port but were actually on password
+                                this.user = this.host;
+                                this.host = null;
+                                this.password = falsy2Null(this.authority.substring(start, cur));
+                            } else {
+                                // we're still on host, but is actually the user
+                                this.user = falsy2Null(this.authority.substring(start, cur));
+                            }
+                            state = State.HOST;
+                            start = cur + 1;
+                        }
+                        // else it's part of the possibly invalid host name or port
+                        break;
+                    // else no special behavior
+                }
             }
         }
-    }
 
-    private parsePath() {
-        if(!this.path) {
-            return;
-        }
-        (this as Writeable).pathSegments = this.path.substring(1).split('/');
-    }
-
-    private parseQuery() {
-        if(!this.query) {
-            return;
+        private parsePath() {
+            if(!this.path) {
+                return;
+            }
+            this.pathSegments = this.path.substring(1).split('/');
         }
 
-        const addQueryParam = (key: string | null, start: number, end: number) => {
-            let mutQps = this.queryParameters as Map<string, Array<string|null>>;
-            let value;
-            if(key == null) {
-                key = this.query!!.substring(start, end);
-                value = null;
-            } else {
-                value = this.query!!.substring(start, end);
+        private parseQuery() {
+            if(!this.query) {
+                return;
             }
-            if(!mutQps.has(key)) {
-                mutQps.set(key, new Array());
+
+            const addQueryParam = (key: string | null, start: number, end: number) => {
+                let value;
+                if(key == null) {
+                    key = this.query!!.substring(start, end);
+                    value = null;
+                } else {
+                    value = this.query!!.substring(start, end);
+                }
+                if(!this.queryParameters.has(key)) {
+                    this.queryParameters.set(key, new Array());
+                }
+                this.queryParameters.get(key)!!.push(value);
             }
-            mutQps.get(key)!!.push(value);
-        }
-        for(let start = 0, cur = 0, k = null;; cur++) {
-            if(cur == this.query.length) {
-                addQueryParam(k, start, cur);
-                break;
-            }
-            switch(this.query.charAt(cur)) {
-                case "=":
-                    if(!k) {
-                        k = this.query.substring(start, cur);
-                        start = cur + 1;
-                    }
-                    break;
-                case "&":
+            for(let start = 0, cur = 0, k = null;; cur++) {
+                if(cur == this.query.length) {
                     addQueryParam(k, start, cur);
-                    start = cur + 1;
-                    k = null;
                     break;
-                //anything besides = and & don't have any special meaning
+                }
+                switch(this.query.charAt(cur)) {
+                    case "=":
+                        if(!k) {
+                            k = this.query.substring(start, cur);
+                            start = cur + 1;
+                        }
+                        break;
+                    case "&":
+                        addQueryParam(k, start, cur);
+                        start = cur + 1;
+                        k = null;
+                        break;
+                    //anything besides = and & don't have any special meaning
+                }
             }
         }
     }
-
-    public toString = () => this.uri;
-    
 }
